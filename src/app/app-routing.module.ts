3792a46b56c16284f55d83922c4892950fe1c12e
Herbax de Mexico <herbax@iMac-de-Herbax.local>
2018-07-13 14:37:06 -0500

    rutas agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { NotfoundPageComponent } from './components/notfound-page/notfound-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'iniciar-sesion', component: LoginPageComponent},
+  {path: 'registro', component: RegisterPageComponent},
+  {path: 'admin', component: AdminPageComponent},
+  {path: '**', component: NotfoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
