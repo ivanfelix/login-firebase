f1d3feac11601829c37b881a45ab5ce2a95beaca
Herbax de Mexico <herbax@iMac-de-Herbax.local>
2018-07-13 14:21:33 -0500

    Componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { NotfoundPageComponent } from './components/notfound-page/notfound-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    AdminPageComponent,
+    NotfoundPageComponent
   ],
   imports: [
     BrowserModule,
